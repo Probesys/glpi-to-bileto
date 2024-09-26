55d3d6bf71a9ce38c94663f665f795ccbc78eb84
Marien Fressinaud <marien.fressinaud@probesys.com>
2024-09-26 15:09:50 +0200

    imp: Detect contracts' max hours equal to zero

--- src/Application.php
+++ src/Application.php
@@ -658,12 +658,22 @@ class Application
             $project_task_id = intval($project_task['id']);
             $contract_id = strval($project_task_id);
 
+            $max_hours = intval($project_task['planned_duration'] / 60 / 60);
+
+            if ($max_hours <= 0) {
+                $this->skipOrInvalid($context, 'Max hours is equal to 0.');
+
+                if ($this->options['skip on error']) {
+                    continue;
+                }
+            }
+
             $contracts[] = [
                 'id' => $contract_id,
                 'name' => $name,
                 'startAt' => $start_at?->format(\DateTimeInterface::RFC3339),
                 'endAt' => $end_at?->format(\DateTimeInterface::RFC3339),
-                'maxHours' => intval($project_task['planned_duration'] / 60 / 60),
+                'maxHours' => $max_hours,
                 'notes' => $contract['comment'],
                 'organizationId' => $organization_id,
                 'timeAccountingUnit' => 30,
